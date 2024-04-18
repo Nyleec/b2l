78635d8f1c51993ee974894a2051e3703526d818
Nyleec <nyleecp@gmail.com>
2024-04-18 07:14:09 -0700

    ref site
    last known code.
    May work may not

---
+++ b2l/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="horizontal-div" id="top">Div 1</div>
+      <div className="horizontal-div" id="left">Div 2</div>
+      <div className="horizontal-div" id="right">Div 3</div>
+      <div className="horizontal-div" id="bottom">Div 4</div>
+    </div>
+  );
+};
+
+export default App;
